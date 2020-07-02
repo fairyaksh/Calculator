bd95d88f7aa395e9fe12152c4535385aed6c27b8
Akshaya S <akshayasatheesh@hotmail.co.uk>
2020-07-02 21:48:48 +0100

    Created function for clear button to reinitialize the display element to 0 when clicked

--- calc.js
+++ calc.js
@@ -1,3 +1,9 @@
 // clear button
 const clearBtn = document.getElementById("clear");
 let display = document.querySelector('calc_display');
+clearBtn.addEventListener("click", clearFunc);
+function clearFunc(e){
+    if (e.target.matches('button')){
+        display.innerHTML = "0";
+    }
+}
