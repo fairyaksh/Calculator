cf6304edccbd39e7106b932233a97e2b275db111
Akshaya S <akshayasatheesh@hotmail.co.uk>
2020-07-04 16:04:21 +0100

    Changed clear button DOM access

--- calc.js
+++ calc.js
@@ -1,6 +1,7 @@
+let display = document.getElementById('display');
+
 // clear button
 const clearBtn = document.getElementById("clear");
-let display = document.querySelector('calc_display');
 clearBtn.addEventListener("click", clearFunc);
 function clearFunc(e){
     if (e.target.matches('button')){
