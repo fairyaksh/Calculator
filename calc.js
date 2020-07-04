908d458b5e53e8c1665c99895e31c39016a3d0ba
Akshaya S <akshayasatheesh@hotmail.co.uk>
2020-07-04 18:52:29 +0100

    When user hits a number key

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 let display = document.getElementById('display');
 
-// clear button
+// Clear button
 const clearBtn = document.getElementById("clear");
 clearBtn.addEventListener("click", clearFunc);
 function clearFunc(e){
@@ -8,3 +8,25 @@ function clearFunc(e){
         display.innerHTML = "0";
     }
 }
+
+// Key presses event listener
+const calculator = document.querySelector('.calculator');
+const keys = document.querySelector(.calc_keys);
+
+keys.addEventListener('click', e){
+    if (e.target.matches('button')){
+        const key = e.target; //capturing the element that triggered the event (aka click)
+        const action = key.dataset.action; //capturing the dataset attribute of this element
+        const keyContent = key.textContent; //capturing the no. of the key clicked
+        const displayedNum = display.textContent //capturing the currently displayed no.
+
+        if (!action) {
+            if (displayedNum === '0'){
+                displayedNum = keyContent;
+            } else {
+                display.textContent = displayedNum + keyContent;
+            }
+        }
+    }
+}
+
