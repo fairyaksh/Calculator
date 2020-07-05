6e7bce86dd58606f7617e26db2e83e47fb9aae8f
Akshaya S <akshayasatheesh@hotmail.co.uk>
2020-07-06 00:11:27 +0100

    Previouskeytype added to number keys

--- calc.js
+++ calc.js
@@ -48,6 +48,7 @@ keys.addEventListener('click', e => {
             } else {
                 display.textContent = displayedNum + keyContent;
             }
+            calculator.dataset.previousKeyType = 'number'
         }
 
         // concatenating decimal after number input
